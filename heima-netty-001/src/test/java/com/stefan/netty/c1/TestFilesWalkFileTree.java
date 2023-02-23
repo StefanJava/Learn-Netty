019f5f9ba546d2ff0bafac5f7ed12b4118a682fd
StefanYang <923775370@qq.com>
2023-02-23 15:56:18 +0800

    Files walkFileTree methods

--- heima-netty-001/src/test/java/com/stefan/netty/c1/TestFilesWalkFileTree.java
+++ heima-netty-001/src/test/java/com/stefan/netty/c1/TestFilesWalkFileTree.java
@@ -14,6 +14,31 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class TestFilesWalkFileTree {
     public static void main(String[] args) throws IOException {
 
+
+
+    }
+
+    private static void m3() throws IOException {
+        Path path = Paths.get("/Volumes/Cynthia/Stefan/Netty网络编程");
+        Files.walkFileTree(path, new SimpleFileVisitor<>() {
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.println(file);
+                Files.delete(file);
+                return super.visitFile(file, attrs);
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                System.out.println("========>" + dir);
+                Files.delete(dir);
+                return super.postVisitDirectory(dir, exc);
+            }
+        });
+    }
+
+    private static void m2() throws IOException {
         AtomicInteger jarCount = new AtomicInteger();
 
         Files.walkFileTree(Paths.get("/Library/Java/JavaVirtualMachines/jdk-17.0.5.jdk/Contents/Home"), new SimpleFileVisitor<>() {
